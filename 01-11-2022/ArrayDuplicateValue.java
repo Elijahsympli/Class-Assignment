45a980b93f55bf753fff81791b43bd6d27a246f6
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-11-01 11:33:05 +0530

    Update ArrayDuplicateValue.java

--- 01-11-2022/ArrayDuplicateValue.java
+++ 01-11-2022/ArrayDuplicateValue.java
@@ -14,7 +14,7 @@ public class ArrayDuplicateValue {
 		  for(int j=0;j<a.length;j++) {
 		  for(int i=j+1;i<a.length;i++) {
 			  if(a[i]==a[j]) {
-				  System.out.println(a[i]+" is the duplicate value an array");
+				  System.out.println(a[i]+" is the duplicate value present in an array");
 			  }
 		  }
 		  }
