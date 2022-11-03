1f6576107dc58c2a05ddefffd26e85278e8ed99b
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-11-03 11:32:31 +0530

    Update IndexArray.java

--- IndexArray.java
+++ IndexArray.java
@@ -1,30 +1,18 @@
 package com.java;
-
-import java.util.Scanner;
-
 //1. Write a Java program to find the index of an array element.
 public class IndexArray {
 public static void main(String args[]) {
-	
-	int l=0;
 	  int a[]={2,1,3,6,7};
 	  System.out.println("Element of an array:");
 	  for(int i=0;i<a.length;i++)
 		  System.out.print(a[i]+" ");
-	  Scanner sc=new Scanner(System.in);
-	  System.out.println("\nThe index of an array:");
-	 
 	  
+	  System.out.println("\nThe index of an array:");
 	  for(int i=0;i<a.length;i++) {
 		
-			  System.out.println(" a["+i+"]="+a[i] );
-		
-		  
-	  }
-	  
-	  
+			  System.out.println(" a["+i+"]="+a[i] );  
+	  } 
 	}
-	
 
 	
 }
